3c0b8bda820b79df8cb1f59fdd7251acde6dc9c0
Sefa Bulak <zckhtln@icloud.com>
2024-05-08 11:58:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+pip3 install google-images-search pandas openpyxl
+
+python3 search.py
---
+++ search.py
@@ -0,0 +1,12 @@
+from google_images_search import GoogleImagesSearch
+import pandas as pd
+
+gis = GoogleImagesSearch('AIzaSyDMNHmTDePsJV7gjktX8Z7ak20OoHlhob4', '24b94c30a21744459')
+
+_search_params = {
+    'q': 'ahinur-baldo-pirinc',
+    'num': 3,
+    'safe': 'off'
+}
+
+gis.search(search_params=_search_params, path_to_dir='/Users/zackhatlen/Desktop/test/')

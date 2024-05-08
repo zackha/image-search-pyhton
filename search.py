8b9cb4bb75b1c719ffb075ae055e0019db0425c6
Sefa Bulak <zckhtln@icloud.com>
2024-05-08 12:46:36 +0200

    Update search.py

--- search.py
+++ search.py
@@ -2,11 +2,11 @@ import os
 import requests
 from serpapi import GoogleSearch
 
-def upload_image_to_imgbb(image_path, api_key):
+def upload_image_to_imgbb(image_path, imgbb_key):
     url = "https://api.imgbb.com/1/upload"
     with open(image_path, 'rb') as file:
         files = {"image": file}
-        params = {"key": api_key}
+        params = {"key": imgbb_key}
         response = requests.post(url, files=files, params=params)
         data = response.json()
         return data.get("data", {}).get("url")
@@ -31,11 +31,14 @@ def process_folder(folder_path, imgbb_key, serpapi_key):
                 uploaded_image_url = upload_image_to_imgbb(image_path, imgbb_key)
                 if uploaded_image_url:
                     results = search_image_on_google_lens(uploaded_image_url, serpapi_key)
-                    if results.get('search_metadata', {}).get('status') == 'Success':
-                        for item in results.get('visual_matches', []):
-                            link = item.get('link')
-                            if link:
-                                print(link)
+                    if results.get('search_metadata', {}).get('status') == 'Success' and results.get('visual_matches'):
+                        first_match = results['visual_matches'][0]  # İlk eşleşmeyi al
+                        link = first_match.get('link')
+                        image = first_match.get('thumbnail')  # Thumbnail resim linki
+                        if link:
+                            print("Link:", link)
+                        if image:
+                            print("Image:", image)
                     else:
                         print("No results found.")
                 else:
@@ -45,7 +48,7 @@ def process_folder(folder_path, imgbb_key, serpapi_key):
 
 # Kullanıcı bu bilgileri doğru şekilde sağlamalıdır.
 IMG_API_KEY = 'fe4095abd2b2d313d039c4d7e28fb628'
-SERP_API_KEY = '21821398356fa120a0910724c0df63944cffc73bff0d13f4db1485ae1c75d3d4'
+SERP_API_KEY = '19d3c31622298ef05734523b5be56d83edbb2db22680cd2e1c1e8b8fc1f85c66'
 FOLDER_PATH = '/Users/zackhatlen/Desktop/test/'
 
 process_folder(FOLDER_PATH, IMG_API_KEY, SERP_API_KEY)
